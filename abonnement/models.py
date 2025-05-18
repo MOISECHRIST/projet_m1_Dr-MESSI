a5d3cc65cae9c84b3bfa8e3475ce596b38eea64a
MOISE CHRISTIAN JUNIOR MEKA <115233333+MOISECHRIST@users.noreply.github.com>
2025-05-19 00:34:28 +0100

    Add user models

--- abonnement/models.py
+++ abonnement/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+class Person(models.Model):
+    LOGIN_STATUS = [("Logout", "Logout"),
+                    ("Login", "Login")]
+
+    USER_TYPE = [
+        ("Customer", "Customer"),
+        ("Worker", "Worker")
+    ]
+    id_person = models.IntegerField(unique=True, blank=False, null=False, primary_key=True)
+    id_user = models.IntegerField(unique=True, blank=False, null=False)
+    login_status = models.CharField(choices=LOGIN_STATUS, max_length=10, blank=False, null=False, default="Login")
+    user_type = models.CharField(max_length=20, choices=USER_TYPE)
+
+class Customer(Person):
+    def save(self, *args, **kwargs):
+        self.user_type = 'Customer'
+        super().save(*args, **kwargs)
+
+
+class Worker(Person):
+    def save(self, *args, **kwargs):
+        self.user_type = 'Worker'
+        super().save(*args, **kwargs)

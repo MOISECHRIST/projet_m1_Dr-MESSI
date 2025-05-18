60de920151de917e7d5e3c0c6a7ee77a00c94832
MOISE CHRISTIAN JUNIOR MEKA <115233333+MOISECHRIST@users.noreply.github.com>
2025-05-19 00:13:12 +0100

    update

--- service_abonnement/settings.py
+++ service_abonnement/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-v@5y72ph5fazmwjfesweoggx7a0&#8ooqs03th3#b2obq=wshx'
+SECRET_KEY = config('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -38,6 +39,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'abonnement',
+    'rest_framework',
+    "rest_framework.authtoken",
+    "dj_rest_auth",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [

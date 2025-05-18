d9f3e137460719cfd590031b5741a22060127490
MOISE CHRISTIAN JUNIOR MEKA <115233333+MOISECHRIST@users.noreply.github.com>
2025-05-18 22:52:01 +0100

    Edit consumer.py

--- offre/consumer.py
+++ offre/consumer.py
@@ -22,6 +22,7 @@ RABBITMQ_PASSWORD = config("RABBITMQ_PASSWORD")
 
 DESTINATAIRE_SERVICE = "offre"
 SENDER = "user_offre"
+SENDER_SERVICE = "user"
 KEYS = [".worker.create_", ".customer.create_", ".user.login_", ".user.logout_", ".worker.delete_", ".customer.delete_"]
 def handle_worker_event(routing_key, data):
     if "create" in routing_key:
@@ -73,7 +74,7 @@ def start_consumer():
     channel.queue_declare(queue=queue_name, durable=True)
 
     # Bind to relevant routing keys
-    keys = [SENDER +i+ DESTINATAIRE_SERVICE for i in KEYS]
+    keys = [SENDER_SERVICE +i+ DESTINATAIRE_SERVICE for i in KEYS]
     for key in keys:
         channel.queue_bind(exchange= SENDER + "_events", queue=queue_name, routing_key=key)
 
